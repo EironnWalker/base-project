4e918f5ef11a92db05515f9477b089ae28e0dc4f
eironn <eironn.walker@gmail.com>
2022-04-26 08:50:50 +0800

    feat: 增加mybatis plus 等分页插件

--- src/main/java/com/eironn/base/BaseApplication.java
+++ src/main/java/com/eironn/base/BaseApplication.java
@@ -2,10 +2,12 @@ package com.eironn.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableWebMvc
 @EnableOpenApi
 public class BaseApplication {
